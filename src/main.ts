dc5e06356a74d862195db72e2c8612dfeeae3a15
zhaopeng <719197352@qq.com>
2018-12-28 09:28:49 +0800

    use logger instead of console

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from '../server.config';
 import { ValidationExceptionFactory } from './exception/validation.exception';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ exceptionFactory: ValidationExceptionFactory }));
   await app.listen(config.port);
-  console.log(`listen on port ${config.port}`);
+  Logger.log(`init finished, listen on port ${config.port}`, 'bootstrap');
 }
 bootstrap();
