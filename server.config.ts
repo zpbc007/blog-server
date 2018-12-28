a2836b4906b486766e0165fcdf0afeaeb87dfe72
zhaopeng <719197352@qq.com>
2018-12-28 09:26:48 +0800

    add config

--- server.config.ts
+++ server.config.ts
@@ -1,13 +1,18 @@
+import { resolve } from 'path';
+
 export const config = {
     port: 3000,
-    jwt: {
+    jwt: { // json web token 相关配置
         secretKey: '_my_private_jwt_key',
         expiresIn: 3600,
     },
-    bcrypt: {
+    bcrypt: { // 密码加密相关配置
         saltRounds: 10,
     },
-    user: {
+    user: { // 用户相关配置
         defaultPass: '123456',
+    },
+    file: { // 文件存储相关配置
+        baseDir: resolve(__dirname, '../blog/doc'),
     },
 };
